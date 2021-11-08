b4a7d8b797fc4daf6b030dc7f2d3beabad73dbd7
Velox <veloxization@gmail.com>
2021-11-08 16:45:13 +0200

    Added tests to achieve 100% branch coverage for varasto.py

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,36 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_negatiivinen_tilavuus_nollautuu(self):
+        testi_varasto = Varasto(-1)
+        self.assertAlmostEqual(testi_varasto.tilavuus, 0)
+
+    def test_negatiivinen_saldo_nollautuu(self):
+        testi_varasto = Varasto(10, -1)
+        self.assertAlmostEqual(testi_varasto.saldo, 0)
+
+    def test_saldo_ei_suurempi_kuin_tilavuus(self):
+        testi_varasto = Varasto(10, 11)
+        self.assertAlmostEqual(testi_varasto.saldo, testi_varasto.tilavuus)
+
+    def test_negatiivista_numeroa_ei_voi_lisata(self):
+        temp_saldo = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, temp_saldo)
+
+    def test_varaston_tilavuutta_ei_voi_ylittaa(self):
+        self.varasto.lisaa_varastoon(11)
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_negatiivista_numeroa_ei_voi_poistaa(self):
+        temp_saldo = self.varasto.saldo
+        self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(self.varasto.saldo, temp_saldo)
+
+    def test_varastosta_ei_voi_poistaa_liikaa(self):
+        self.varasto.lisaa_varastoon(5)
+        self.assertAlmostEqual(self.varasto.ota_varastosta(6), 5)
+
+    def test_str_palautus_toimii(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
